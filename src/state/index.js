943f5a7f9595a32a54f6c6a41cf971f432871b8b
陈彦成 <342930982@qq.com>
2016-09-22 23:50:05 +0800

    状态单独文件

---
+++ src/state/index.js
@@ -0,0 +1,15 @@
+export default {
+    header: {
+      type: 'all',
+      open: false
+    },
+    list: {
+      loading: false,
+      data: [],
+      page: 1
+    },
+    topic: {
+      data:{},
+      loading: true
+    }
+}
